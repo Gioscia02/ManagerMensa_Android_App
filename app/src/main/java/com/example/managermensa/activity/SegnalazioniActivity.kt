07fb70c502fc40b026d7b1f2c0e5635c7f59fc31
Giorgio Sciacca <132167352+Gioscia02@users.noreply.github.com>
2024-05-26 19:04:03 +0200

    bug risolti e viewmodel implementata

--- app/src/main/java/com/example/managermensa/activity/SegnalazioniActivity.kt
+++ app/src/main/java/com/example/managermensa/activity/SegnalazioniActivity.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.widget.RadioButton
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -27,38 +28,7 @@ class SegnalazioniActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySegnalazioniBinding
 
-    private val itemList = MutableLiveData<ArrayList<Utente>>()
-    init {
-        itemList.value = ArrayList()
-    }
-
-    val utenteSelezionato = MutableLiveData<Utente?>()
-    init {
-        utenteSelezionato.value = null
-    }
-
-    val position = MutableLiveData<Int>()
-    init {
-        position.value = -1
-    }
-
-    val success = MutableLiveData<Boolean>()
-    init {
-        success.value = false
-    }
-
-    fun getItemList(): LiveData<ArrayList<Utente>> {
-        return itemList
-    }
-
-    fun addItem(item: Utente) {
-        itemList.value?.add(item)
-    }
-
-    fun removeItem(item: Utente) {
-        itemList.value?.remove(item)
-    }
-
+   val viewModel : SharedViewModel by viewModels()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -91,7 +61,7 @@ class SegnalazioniActivity : AppCompatActivity() {
                 val selectedText = selectedRadioButton.text
 
 
-                insetItem(selectedText.toString(), binding.editTextSegnalazione.text.toString())
+                viewModel.insertSegnalazione(binding,selectedText.toString(), binding.editTextSegnalazione.text.toString())
 
             } else {
                 showToast("Compila tutti i campi")
@@ -108,45 +78,6 @@ class SegnalazioniActivity : AppCompatActivity() {
     }
 
 
-    //Invio segnalazione
-    fun insetItem(argomento: String, testo: String) {
-        val gson = Gson()
-        val string  =
-            "{\"argomento\": \"$argomento\", \"testo\": \"$testo\"}"
-
-        val json = gson.fromJson(string, JsonObject::class.java)
-        Client.retrofit.insertSegnalazione(json).enqueue(
-            object : Callback<JsonObject> {
-                override fun onResponse(p0: Call<JsonObject>, p1: Response<JsonObject>) {
-                    if (p1.isSuccessful) {
-                        val risposta = JsonArray()
-                        risposta.add(p1.body())
-                        Log.v("risposta", risposta.toString())
-                        addItem(parseJsonToModel(risposta)[0])
-                        success.value = true
-
-                        //Pulizia dei campi dopo invio segnalazione
-                        binding.editTextSegnalazione.text.clear()
-                        binding.argomentiSegnalazione.clearCheck()
-
-                        showToast("Segnalazione inviata")
-                    }
-                    else{
-                        success.value = false
-                        println("errato")
-                    }
-                }
-                override fun onFailure(p0: Call<JsonObject>, p1: Throwable) {
-                    success.value = false
-                }
-            }
-        )
-    }
-
-    private fun parseJsonToModel(jsonString: JsonArray): ArrayList<Utente> {
-        val gson = GsonBuilder().setDateFormat("yyyy-MM-dd").create()
-        return gson.fromJson(jsonString, object : TypeToken<ArrayList<Utente>>() {}.type)
-    }
 
     private fun showToast(message: String) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
